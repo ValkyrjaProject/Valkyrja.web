dea37d8991eb7ff90200bf74fae8a3e4ea2b5ad7
SpyTec <spytec13@gmail.com>
2017-02-23 17:58:32 +0100

    Changed text to homepage regarding uptime

--- www/resources/views/home.blade.php
+++ www/resources/views/home.blade.php
@@ -12,7 +12,9 @@
         <p>
             Botwinder is highly customizable Community Management bot, a large package of advanced moderation tools, role assignment, antispam, polls, giveaways, events, custom commands & aliases, scheduled messages & commands, and even livestream notifications or meeting management systems. Botwinder will keep track of your users and their naughty activities, it can log all kinds of things and get rid of spambots. Comes with <b>cookies</b> and friendly dev team, who will always help and answer your questions with a smile!
             <br /><br />
-            Stability and 99.965% uptime guaranteed!
+            <s>Stability and 99.95% uptime guaranteed!</s>
+			<br />
+			Redesigning infrastructure with some maintenance once in a while.
         </p>
     </div>
 </section>
