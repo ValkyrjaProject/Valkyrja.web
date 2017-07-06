fe06ec69444bfa6dc1a19ce44aa5994d961c8739
SpyTec <spytec13@gmail.com>
2017-07-06 12:36:05 +0200

    Added more exception handling

---
+++ www/resources/views/errors/500.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', '500 Server error')
+
+@section('content')
+    <div class="middle-container">
+        <div class="float-middle section-big">
+            <h1>Internal Server Error</h1>
+            <p>
+                Something went wrong on our end. Try to refresh or clear cache
+            </p>
+        </div>
+    </div>
+@endsection
