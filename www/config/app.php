00bb7e2d601d72edbff6515a6d49f94d684a53f5
Eric <SpyTec@users.noreply.github.com>
2017-07-10 22:41:08 +0200

    Fixed log level

--- www/config/app.php
+++ www/config/app.php
@@ -109,7 +109,7 @@ return [
     */
 
     'log' => env('APP_LOG', 'single'),
-    'log-level' => env('APP_LOG_LEVEL', 'error'),
+    'log_level' => env('APP_LOG_LEVEL', 'error'),
 
     /*
     |--------------------------------------------------------------------------
