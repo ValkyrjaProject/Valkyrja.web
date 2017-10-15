3c175e357b91fb10df135409dbffd876d56e82e8
SpyTec <spytec13@gmail.com>
2017-10-16 00:42:06 +0200

    Added maintenance page.

--- www/app/Http/routes.php
+++ www/app/Http/routes.php
@@ -13,7 +13,10 @@
 
 Route::get('/callback', 'TestController@index');
 
-Route::group(['prefix' => 'config'], function () {
+Route::get('/config', function () {
+    return view('errors.unavailable');
+});
+/*Route::group(['prefix' => 'config'], function () {
     Route::get('/login', 'ConfigController@login')->name('login');
     Route::get('/logout', 'ConfigController@logout')->name('logout');;
     Route::post('/edit', 'ConfigController@redirectConfig');
@@ -23,11 +26,17 @@ Route::group(['prefix' => 'config'], function () {
         Route::get('/edit/{serverId}', 'ConfigController@displayConfig')->name('editConfig');
         Route::post('/save/{serverId}', 'ConfigController@saveConfig');
     });
-});
+});*/
 
 Route::get('/', 'HomeController@index');
-Route::get('/docs', 'FeaturesController@index');
-Route::get('/features', 'FeaturesController@index');
+Route::get('/docs', function () {
+    return view('errors.unavailable');
+});
+Route::get('/features', function () {
+    return view('errors.unavailable');
+});
+/*Route::get('/docs', 'FeaturesController@index');
+Route::get('/features', 'FeaturesController@index');*/
 Route::get('/updates', 'ChangelogController@index');
 Route::get('/contribute', 'ContributeController@index');
 Route::get('/help', 'HelpController@index');
@@ -36,7 +45,7 @@ Route::get('/invite', 'InviteController@index');
 Route::get('/meetings/{channelID}/{meetingName}', 'MeetingsController@index');
 Route::any('/meetings/{channelID}/{meetingName}/meeting', 'MeetingsController@getMeeting');
 
-Route::group(['middleware' => ['authorizeDiscord', 'authorizeAdmins']], function () {
+/*Route::group(['middleware' => ['authorizeDiscord', 'authorizeAdmins']], function () {
     Route::get('logs', '\Rap2hpoutre\LaravelLogViewer\LogViewerController@index');
 });
 Route::group(['prefix' => 'api', 'middleware' => ['authorizeDiscord']], function () {
@@ -49,4 +58,4 @@ Route::group(['prefix' => 'api', 'middleware' => ['authorizeDiscord']], function
     Route::get('data/{serverId}/{configAttribute}', 'ApiController@getData');
 
     Route::get('botwinderCommands', 'ApiController@getBotwinderCommands');
-});
+});*/
---
+++ www/resources/views/errors/unavailable.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+
+@section('title', 'Error occurred')
+
+@section('content')
+    <div class="middle-container">
+        <div class="float-middle section-big">
+            <h1>Notice!</h1>
+            <p>
+                The documentation and Botwinder's configuration will not be available for a few days, due to the release of the new, redesigned version of <code>Botwinder Mk.III</code>.
+            </p>
+        </div>
+    </div>
+@endsection
