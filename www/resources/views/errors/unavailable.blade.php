56a4b2e76d36e1cb593385c873d85a33ac33ce04
SpyTec <spytec13@gmail.com>
2017-10-16 00:59:07 +0200

    Added invite link

--- www/resources/views/errors/unavailable.blade.php
+++ www/resources/views/errors/unavailable.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.master')
 
-@section('title', 'Error occurred')
+@section('title', 'Unavailable')
 
 @section('content')
     <div class="middle-container">
@@ -8,6 +8,9 @@
             <h1>Notice!</h1>
             <p>
                 The documentation and Botwinder's configuration will not be available for a few days, due to the release of the new, redesigned version of <code>Botwinder Mk.III</code>.
+            </p>
+            <p>
+                Follow the <code>#news</code> channel on our [Support server](http://support.botwinder.info/)
             </p>
         </div>
     </div>
