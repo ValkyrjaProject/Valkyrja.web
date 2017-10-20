60c76538c9925fab245a0e76bf9c1cc57c478c6d
DokaDoka <31368547+DokaDoka@users.noreply.github.com>
2017-10-21 04:00:14 +1100

    Update invite page

--- www/resources/views/invite.blade.php
+++ www/resources/views/invite.blade.php
@@ -27,17 +27,16 @@
     <section class="container">
         <div class="col-xs-12">
             <div class="row">
-                <h1>Botwinder Mk.II</h1>
+                <h1>Botwinder Mk.III</h1>
                 {{--<div>
                     <img src="/img/jefi-discord.jpg" alt="Botwinder" class="square rounded-circle img-thumbnail" width="150px">
                 </div>--}}
             </div>
             <p class="row">
-              You can use Botwinder Mk.II for <b>one day trial period</b>, after which the bot will automatically leave your server, if you do not meet at least <b>one of the requirements</b> below.
+              You can use Botwinder Mk.III for <b>one day trial period</b>, after which the bot will automatically leave your server, if you do not meet at least <b>one of the requirements</b> below.
               <br />
               <b>Requirements</b> <i>(one of)</i><ul>
-                <li><a href="https://www.patreon.com/Botwinder" target="_blank">Patreon</a> Subscribers at 3$ or more</li>
-                <li><a href="https://discordapp.com/partners" target="_blank">Discord Partner</a> with <a href="https://i.imgur.com/6c8oUoD.png" target="_blank">VIP voice servers</a> up to 20 000 members.</li>
+                <li><a href="https://www.patreon.com/Botwinder" target="_blank">Patreon</a> Subscribers at 10$ or more</li>
                 <li>Handpicked partners - unique communities with worthy cause, such as open source projects, Linux or other geeky places, and any Elite Dangerous server. <i>(talk to our <a href="/team">PR team</a>)</i></li>
               </ul>
             </p>
