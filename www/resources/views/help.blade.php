1f07ca59944e7f11667f8598a94f60acc465f792
Eric <SpyTec@users.noreply.github.com>
2018-03-25 18:47:45 +0200

    Updated email contacts

--- www/resources/views/help.blade.php
+++ www/resources/views/help.blade.php
@@ -10,12 +10,7 @@
             Get in touch with us on <b><a href="http://support.botwinder.info" target="_blank">our Discord server</a></b>, where you can ask any questions and our <b>Support team</b> or Developers will try to answer them with a smile <b>=)</b>
         </p>
         <p>
-            <b>Email contacts:</b>
-            <ul>
-                <li>General: <span class="email"><a href="emailto:team@botwinder.info">team@botwinder.info</a></span></li>
-                <li>Support: <span class="email"><a href="emailto:support@botwinder.info">support@botwinder.info</a></span></li>
-                <li>Public Relations: <span class="email"><a href="emailto:pr@botwinder.info">pr@botwinder.info</a></span></li>
-            </ul>
+            <b>Email contacts:</b> <span class="email"><a href="emailto:team@botwinder.info">team@botwinder.info</a></span> (Not for questions regarding the bot.)
         </p>
         <p>
             Please do not PM individual team members including Rhea. Join the <a href="https://discord.gg/XgVvkXx">Discord Server</a>, take a look at <code>FAQ</code>, recent entries in <code>news</code>, and then ask in the <code>support</code> channel
