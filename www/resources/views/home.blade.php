ae391b839ef2e7655e315d7478d3b7912d05a12a
Radka Janek <radka.janek@redhat.com>
2018-10-03 20:48:40 +0200

    *home

--- www/resources/views/home.blade.php
+++ www/resources/views/home.blade.php
@@ -10,7 +10,7 @@
     <div class="col-md-6 col-lg-8">
         <h1>Botwinder - The Discord Bot</h1>
         <p>
-            Botwinder is highly customizable Community Management bot, a large package of advanced moderation tools, role assignment, temporary channels, antispam, user verification, polls, custom commands & aliases, scheduled messages & commands, and even meeting management systems. Botwinder will keep track of your users and their naughty activities, it can log all kinds of things and get rid of spambots. Comes with <b>cookies</b> and friendly dev team, who will always help and answer your questions with a smile!
+            Botwinder is highly customizable Community Management bot, a large package of advanced moderation tools, role assignment, temporary channels, antispam, user verification, custom commands & aliases, user profiles and many other community related features. Botwinder will keep track of your users and their naughty activities, it can log all kinds of things and get rid of spambots. Comes with <b>cookies</b> and friendly dev team, who will always help and answer your questions with a smile!
             <br /><br />
             Stability and 99.95% uptime guaranteed!
         </p>
