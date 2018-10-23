273f947c05728f189eae9a20177bd88f43661044
Eric Gustavsson <egustavs@redhat.com>
2018-10-23 16:54:40 +0200

    Added tests for Panel List item and search components

--- www/tests/JS/components/Shared/Structure/PanelList/PanelListItem.spec.js
+++ www/tests/JS/components/Shared/Structure/PanelList/PanelListItem.spec.js
@@ -1,45 +1,50 @@
-import sinon from "sinon";
 import {shallowMount} from "@vue/test-utils";
-import Vue from "vue";
-import Vuex from "vuex";
 import {expect} from "chai";
 import PanelListItem from "components/shared/structure/PanelList/PanelListItem";
 
-let localVue = Vue.use(Vuex);
-
 describe("PanelListItem", function () {
     let wrapper;
-    let actions;
-    let store;
-    let state;
-    let getters;
     let propsData;
 
     beforeEach(function () {
-        propsData = {};
+        propsData = {
+            item: {
+                toString() {
+                    return "Lorem ipsum dolor sit amet."
+                }
+            }
+        };
 
-        actions = {};
-        state = {};
-        getters = {};
-        store = new Vuex.Store({
-            state,
-            actions,
-            getters
-        });
-        wrapper = shallowMount(PanelListItem, {propsData, store, localVue});
+        wrapper = shallowMount(PanelListItem, {propsData});
     });
 
-    it("should have 'a' as root-element");
+    it("should have element 'a' with .panel-block class as root-element", function () {
+        expect(wrapper.is("a.panel-block")).to.be.true;
+    });
 
-    it("should emit 'click' with item on root-element click");
+    it("should emit 'click' with item on root-element click", function () {
+        expect(wrapper.emitted().click).to.be.undefined;
+        wrapper.find("a.panel-block").vm.$emit("click");
+        expect(wrapper.emitted().click.length).to.equal(1);
+    });
 
-    it("should have optional 'itemIcon' prop");
+    it("should have Material Design icon on left side if itemIcon prop is set", function () {
+        wrapper.setProps({itemIcon: "test"});
+        expect(wrapper.find("a .panel-icon i").classes()).to.contain("mdi-test")
+    });
 
-    it("should have required 'item' prop");
+    it("should shorten 'item' toString value to 25 characters directly under root-element", function () {
+        expect(wrapper.find("a.panel-block").text()).to.equal(propsData.item.toString().substring(0, 25))
+    });
 
-    it("should have Material Design icon on left side if itemIcon prop is set");
-
-    it("should shorten 'item' toString value to 25 characters directly under root-element");
-
-    it("should trim whitespace of 'item' toString");
+    it("should trim whitespace of 'item' toString", function () {
+        wrapper.setProps({
+            item: {
+                toString() {
+                    return " test "
+                }
+            }
+        });
+        expect(wrapper.find("a.panel-block").text()).to.equal("test");
+    });
 });
--- www/tests/JS/components/Shared/Structure/PanelList/PanelListSearch.spec.js
+++ www/tests/JS/components/Shared/Structure/PanelList/PanelListSearch.spec.js
@@ -1,51 +1,60 @@
-import sinon from "sinon";
 import {shallowMount} from "@vue/test-utils";
-import Vue from "vue";
-import Vuex from "vuex";
 import {expect} from "chai";
 import PanelListSearch from "components/shared/structure/PanelList/PanelListSearch";
 
-let localVue = Vue.use(Vuex);
-
 describe("PanelListSearch", function () {
     let wrapper;
-    let actions;
-    let store;
-    let state;
-    let getters;
     let propsData;
 
     beforeEach(function () {
-        propsData = {};
-
-        actions = {};
-        state = {};
-        getters = {};
-        store = new Vuex.Store({
-            state,
-            actions,
-            getters
-        });
-        wrapper = shallowMount(PanelListSearch, {propsData, store, localVue});
+        propsData = {
+            value: ""
+        };
+        wrapper = shallowMount(PanelListSearch, {propsData});
     });
 
-    it("should have div.panel-block as root element");
+    it("should have div.panel-block as root element", function () {
+        expect(wrapper.find("div.panel-block").exists()).to.be.true;
+    });
 
-    it("should have one child with .field.has-addons.control classes under .panel-block");
+    it("should have one child with .field.has-addons.control classes under .panel-block", function () {
+        expect(wrapper.findAll("div.panel-block > .field.has-addons.control").length).to.equal(1);
+    });
 
-    it("should have two .control children under .has-addons");
+    it("should have two .control children under .has-addons", function () {
+        expect(wrapper.findAll(".has-addons > .control").length).to.equal(2);
+    });
 
-    it("should have a small full-width search input on left side");
+    it("should have a small full-width search input on left side", function () {
+        expect(wrapper.find(".has-icons-left").classes()).to.contain("is-expanded");
+    });
 
-    it("should have 'Search' as placeholder for search input");
+    it("should have 'Search' as placeholder for search input", function () {
+        expect(wrapper.find(".input").attributes().placeholder).to.equal("Search");
+    });
 
-    it("should emit 'input' on search input change");
+    it("should emit 'input' with value on search input change", function () {
+        expect(wrapper.emitted().input).to.be.undefined;
+        wrapper.find(".input").setValue("test");
+        expect(wrapper.emitted().input.length).to.equal(1);
+        expect(wrapper.emitted().input[0]).to.deep.equal(["test"]);
+    });
 
-    it("should have a small magnifying class search input on left side of input");
+    it("should have a small magnifying class search input on left side of input", function () {
+        expect(wrapper.find(".has-icons-left > .icon").classes()).to.contain("is-left");
+    });
 
-    it("should have a small close button on right side with .is-info class");
+    it("should have a small close button on right side with .is-info class", function () {
+        expect(wrapper.findAll(".field > .control").at(1).find(".button").classes()).to.contain("is-small");
+    });
 
-    it("should emit 'clear' on close button click");
+    it("should emit 'clear' on close button click", function () {
+        expect(wrapper.emitted().clear).to.be.undefined;
+        wrapper.find(".button").trigger("click");
+        expect(wrapper.emitted().clear.length).to.equal(1);
+    });
 
-    it("should have cross icon for close button");
+    it("should have cross icon for close button", function () {
+        expect(wrapper.findAll(".field > .control").at(1).find(".button .mdi").classes()).to.contain("mdi-close");
+    });
 });
