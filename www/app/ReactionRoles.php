d0d9c95f7af3d757f98aa9c147afa6d88e23fb65
Eric Gustavsson <egustavs@redhat.com>
2018-10-25 12:46:13 +0200

    +last commit

---
+++ www/app/ReactionRoles.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Model;
+
+class ReactionRoles extends Model
+{
+    public $timestamps = false;
+    public $incrementing = false;
+    protected $primaryKey = ['serverid', 'messageid', 'emoji'];
+    protected $fillable = [
+        'roleid',
+        'messageid',
+        'emoji',
+    ];
+
+    protected $casts = [
+        'roleid' => 'string',
+        'messageid' => 'string',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function setKeysForSaveQuery(Builder $query)
+    {
+        $query
+            ->where($this->primaryKey[0], '=', $this->getAttribute($this->primaryKey[0]))
+            ->where($this->primaryKey[1], '=', $this->getAttribute($this->primaryKey[1]))
+            ->where($this->primaryKey[2], '=', $this->getAttribute($this->primaryKey[2]));
+        return $query;
+    }
+
+    public function getForeignKey()
+    {
+        return 'serverid';
+    }
+}
