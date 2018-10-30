105157b2d8ff1d838bb1439b42d58a74e4bae61c
Eric Gustavsson <egustavs@redhat.com>
2018-10-30 16:37:46 +0100

    Added some tests for ProfileEditor

--- www/tests/JS/components/EditGuild/Sections/Social/ProfileEditor/ProfileEditor.js
+++ www/tests/JS/components/EditGuild/Sections/Social/ProfileEditor/ProfileEditor.js
@@ -4,6 +4,7 @@ import Vue from "vue";
 import Vuex from "vuex";
 import {expect} from "chai";
 import ProfileEditor from "components/EditGuild/Sections/Social/ProfileEditor/ProfileEditor";
+import PanelList from "components/shared/structure/PanelList/PanelList";
 
 let localVue = Vue.use(Vuex);
 
@@ -13,21 +14,83 @@ describe("ProfileEditor", function () {
     let store;
     let state;
     let getters;
-    let propsData;
+    let profileArray = [];
 
     beforeEach(function () {
-        propsData = {};
-
-        actions = {};
-        state = {};
-        getters = {};
+        actions = {
+            setSelectedProfile: sinon.stub(),
+            changeField: sinon.stub(),
+            addProfile: sinon.stub(),
+        };
+        state = {
+            selectedProfile: null
+        };
+        let profiles = sinon.stub();
+        profiles.returns(profileArray);
+        getters = {
+            profiles: profiles
+        };
         store = new Vuex.Store({
-            state,
-            actions,
-            getters
+            modules: {
+                profileEditor: {
+                    namespaced: true,
+                    state,
+                    actions,
+                    getters
+                }
+            }
         });
-        wrapper = shallowMount(ProfileEditor, {propsData, store, localVue});
+        wrapper = shallowMount(ProfileEditor, {store, localVue});
     });
 
-    it("should ");
+    it("should have two columns", function () {
+        expect(wrapper.findAll(".box .columns > .column").length).to.equal(2);
+    });
+
+    it("should have profiles list on left column", function () {
+        expect(wrapper.findAll(".box .columns > .column").at(0).props().value).to.equal(profileArray);
+    });
+
+    it("should have selected profile options on right column", function () {
+        let column = wrapper.findAll(".box .columns > .column").at(1);
+        expect(column.find("div.panel.panel-parent").exists()).to.be.true;
+    });
+
+    it("should have a PanelList with list of profiles", function () {
+        expect(wrapper.find(PanelList).props().value).to.deep.equal([]);
+    });
+
+    it("should have a PanelList with add button", function () {
+        expect(wrapper.find(PanelList).props().addButton).to.be.true;
+    });
+
+    it("should disable inputs if profile is not selected", function () {
+        state.selectedProfile = null;
+        let inputs = wrapper.findAll("input");
+        expect(inputs.length, "Not all profile elements were found").to.equal(5);
+        for (let i = 0; i < inputs.length; i++) {
+            expect(inputs.at(i).attributes("disabled"), "should be undefined if input is enabled").to.not.be.undefined;
+        }
+    });
+
+    it("should enable inputs if profile is selected", function () {
+        state.selectedProfile = {};
+        let inputs = wrapper.findAll("input");
+        expect(inputs.length, "Not all profile elements were found").to.equal(5);
+        for (let i = 0; i < inputs.length; i++) {
+            expect(inputs.at(i).attributes("disabled"), "should be undefined if input is enabled").to.be.undefined;
+        }
+    });
+
+    it("should have 'option' input that changes selected profile");
+
+    it("should have 'option_alt' input that changes selected profile");
+
+    it("should have 'label' input that changes selected profile");
+
+    it("should have 'property_order' input that changes selected profile");
+
+    it("should have 'inline' input that changes selected profile");
+
+    it("should create ProfileOption class when clicking new profile button");
 });
