84cd4d645c26e15e60612819cac57fb2b25b99e6
Radka Janek <radka.janek@redhat.com>
2019-06-20 20:04:58 +0200

    added youtubie

--- www/resources/views/config/display_servers.blade.php
+++ www/resources/views/config/display_servers.blade.php
@@ -8,8 +8,13 @@
 		<h1>Select server</h1>
 		<p>
 			Hi {{ $user->username }}!<br />
+                        Check out this video how to set-up a secure Discord server with Valkyrja!<br />
+                        <a href="https://youtu.be/KDQeN7FXO-4">youtu.be/KDQeN7FXO-4</a>
+		</p>
+		<p>
 			Required permissions to be able to configure Valkyrja are <code>Administrator</code> & <code>ManageServer</code>, and you need to already have it on your server, before you can configure anything. <a href="/invite">Invite the bot!</a></p>
 		</p>
+		<p>
 			Which server would you like to configure?
 		</p>
 		<form action="{{ url('config/edit') }}" class="form-inline" method="post">
