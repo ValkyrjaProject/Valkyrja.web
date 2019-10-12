3f84486ebfffb167ceb113871d0a9d4c29c361a4
RheaAyase <radka.janek@redhat.com>
2019-10-12 09:59:23 +0200

    update help page

--- www/resources/views/help.blade.php
+++ www/resources/views/help.blade.php
@@ -10,10 +10,10 @@
             Get in touch with us on <b><a href="https://discord.gg/XgVvkXx" target="_blank">our Discord server</a></b>, where you can ask any questions and our <b>Support team</b> or Developers will try to answer them with a smile <b>=)</b>
         </p>
         <p>
-            <b>Email contacts:</b> <span class="email"><a href="emailto:team@valkyrja.app">team@valkyrja.app</a></span> (Not for questions regarding the bot.)
+            Please do not PM individual team members including Rhea. Join the <a href="https://discord.gg/XgVvkXx">Discord Server</a>, take a look at <code>FAQ</code>, recent entries in <code>#news</code>, and then ask in the <code>#support</code> channel.
         </p>
         <p>
-            Please do not PM individual team members including Rhea. Join the <a href="https://discord.gg/XgVvkXx">Discord Server</a>, take a look at <code>FAQ</code>, recent entries in <code>#news</code>, and then ask in the <code>#support</code> channel.
+            Should you wish to have any and all the data related to your Discord account removed from our database, please contact Rhea#1234 directly.
         </p>
     </div>
 </section>
